2379a9b3a62cbfa0a9bb2efe1d125947b72f1930
Pawan199925 <pawanarunborse7@gmail.com>
2025-04-03 13:19:18 +0530

    upadate code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-let allUserInfo = []
-let regform = document.querySelector (".reg-form");
-let allInput = regform.querySelectorAll ("input");
-console.log ("allInput");
-regfrom.onsubmit = ("e") = > {
-    e.preventDefault()
+let allUserInfo = [];
+let regform = document.querySelector(".reg-form");
+let allInput = regform.querySelectorAll("input");
+console.log(allInput);
+regform.onsubmit = (e) => {
+    e.preventDefault();
     let data = {
-        Fullname : allInput[ 0 ]. value,
-        hotelName : allInput[ 1 ] .value,
+        fullname : allInput[0].value,
+        hotelName : allInput[1].value,
     }
     alert();
-
 }
