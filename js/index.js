593547988926dbbb6b86ccec5ac17e23a25c0faa
Pawan199925 <pawanarunborse7@gmail.com>
2025-04-04 02:42:22 +0530

    upadate Registration data

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
 let allUserInfo = [];
-let regform = document.querySelector(".reg-form");
-let allInput = regform.querySelectorAll("input");
-console.log(allInput);
-regform.onsubmit = (e) => {
-    e.preventDefault();
-    let data = {
-        fullname : allInput[0].value,
-        hotelName : allInput[1].value,
+let regForm = document.querySelector(".reg-form");
+let allInput = regForm.querySelectorAll("input");
+
+if(localStorage.getItem("allUserInfo") != null)
+{
+    allUserInfo = JSON.parse(localStorage.getItem("allUserInfo"))
+}
+console.log(allUserInfo);
+
+
+    regForm.onsubmit = (e) =>{
+    e.preventDefault()
+    let checkEmail = allUserInfo.find((data)=>{
+        return data.email == allInput[4].value
+    })
+    if(checkEmail == undefined)
+    {
+        let data = {};
+    for(let el of allInput)
+    {
+        let key = el.name;
+        data[key] = el.value
     }
-    alert();
+    allUserInfo.push(data);
+    localStorage.setItem("allUserInfo",JSON.stringify(allUserInfo));
+    swal("Good Job !", 'Registration Success !','Success');
+    }
+    else
+    {
+        swal("Failed !",'Email already register !','warning');
+    }
 }
